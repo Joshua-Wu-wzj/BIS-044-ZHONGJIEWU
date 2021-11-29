0c170596aedca7f8d1ea1cfde0ac9c7edca3704f
Joshua-Wu-wzj <71334474+Joshua-Wu-wzj@users.noreply.github.com>
2021-11-29 15:20:34 -0500

    Add files via upload

--- Assignment 5.R
+++ Assignment 5.R
@@ -3,7 +3,7 @@ dataset2 <- read_csv("TBQ05.20211116133909.csv")
 if (!require("tidyverse")) install.packages("tidyverse")
 library(tidyverse)
 
+VALUE <- dataset2[10]
+as.numeric(unlist(VALUE))
 dataset2[is.na(dataset2)] <- "0"
-
-as.numeric((dataset2[VALUE]))
 View(dataset2)
